4685837cd353e4a0aa77db1ac6f4ba868c441f12
mourasuporte24 <mourasuporte24@gmail.com>
2022-05-12 23:40:15 -0300

    Find Movies

---
+++ backend/src/main/java/com/contechsistemas/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,12 @@
+package com.contechsistemas.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.contechsistemas.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie,Long> {
+}
+	
+	
+
+
