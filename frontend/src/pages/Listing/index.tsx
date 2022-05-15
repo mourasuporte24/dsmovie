7f74db679f9cfafeae721756619b0f20ce27db33
mourasuporte24 <mourasuporte24@gmail.com>
2022-05-15 20:23:15 -0300

    Props-com erro

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,76 +1,54 @@
 import axios from "axios";
-
-import{useState, useEffect} from "react";
-import Pagination from "components/Pagination";
+import { useState, useEffect } from "react";
 import MovieCard from "components/MovieCard";
-import {BASE_URL} from "utils/request";
-import {MoviePage} from "types/movie";
-
+import Pagination from "components/Pagination";
+import { BASE_URL } from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-const [pageNumber, setpageNumber] = useState(0);
+    const [pageNumber, setPageNumber] = useState(0);
 
-useEffect(()=>{
-    axios.get(`${BASE_URL}/movies?size=&page=1`)
-    .then(response => {
-        const data = response.data as MoviePage;
-        console.log(data);
-        setpageNumber(data.number);     
+    const [page, setPage] = useState<MoviePage> ({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
     });
-},[]);
-    
-    
-   
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPage(data);
+            });
+    }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
+
     return (
-        < >
-        <p>{pageNumber}</p>
+        <>
             <Pagination />
 
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        <MovieCard />
-                    </div>
-                    
-                               
-                                      
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
                 </div>
             </div>
-    </>
+        </>
     );
 }
+
 export default Listing;
