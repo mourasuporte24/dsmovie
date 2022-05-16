c50d9f842495bc75766760c47a128d687e43aa0e
mourasuporte24 <mourasuporte24@gmail.com>
2022-05-15 23:09:21 -0300

    Show score

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,15 +1,8 @@
 import FormCard from 'components/FormCard';
 import {useParams } from 'react-router-dom';
-
-
-
-
-
 function Form( ){
 
-
 const params = useParams();
-
 
     return (
     
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -35,20 +35,20 @@ function Listing() {
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
-            <div className="container">
-                <div className="row">
-                    {page.content.map(movie => (
-                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                            <MovieCard movie={movie} />
-                        </div>
-                    )
-                    )}
-                </div>
+<div className="container">
+    <div className="row">
+        {page.content.map(movie => (
+            <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                <MovieCard movie={movie} />
             </div>
-        </>
-    );
+        )
+        )}
+    </div>
+</div>
+</>
+);
 }
 
 export default Listing;
